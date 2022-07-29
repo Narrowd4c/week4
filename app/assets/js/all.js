d9bf2ee9d28b3a19d77376861286d1eec52fa8b6
john <aarontu803@gmail.com>
2022-07-29 20:37:12 +0800

    Update 2022-07-29T12:37:05.773Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-const menu=document.querySelector(".menu")
-function menuChange(e) {
-  e.classList.toggle("change");
-  menu.classList.toggle("menu-display")
-}
---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var menu = document.querySelector(".menu");
+
+function menuChange(e) {
+  e.classList.toggle("change");
+  menu.classList.toggle("menu-display");
+}
+//# sourceMappingURL=all.js.map
