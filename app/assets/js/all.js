ef21212c9b041373c75824a2d5eef66747b79ac3
john <aarontu803@gmail.com>
2022-07-29 20:34:57 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+const menu=document.querySelector(".menu")
+function menuChange(e) {
+  e.classList.toggle("change");
+  menu.classList.toggle("menu-display")
+}
